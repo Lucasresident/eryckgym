4d7af68019dadba8b672cd458f8102876e2923be
lucasresident <playlukas06@gmail.com>
2024-04-05 09:44:52 -0300

    criando nova model

--- treinos/models.py
+++ treinos/models.py
@@ -1,8 +1,23 @@
 from django.db import models
 
-
 class Exercicio(models.Model):
     nome = models.CharField(max_length=250)
     descricao = models.CharField(max_length=500)
     em_equipamento = models.BooleanField(default=True)
     idade_minima_aluno = models.PositiveIntegerField(default=12)
+
+    def __str__(self):
+            return f'Nome: {self.nome} | Descrição: {self.descricao} | Equipamento: {self.em_equipamento} | Idade Mínima: {self.idade_minima_aluno}'
+
+class Treino(models.Model):
+    nome = models.CharField(max_length=100)
+    exercicios = models.ManyToManyField(Exercicio, through='Item')
+
+    def __str__(self):
+            return f'Nome: {self.nome} | Exercicio: {self.exercicios}'
+
+class Item(models.Model):
+    exercicio = models.ForeignKey(Exercicio, on_delete=models.CASCADE)
+    treino = models.ForeignKey(Treino, on_delete=models.CASCADE)
+
+   
